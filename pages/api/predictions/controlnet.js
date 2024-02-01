6c0be4414b65e099ceb84e728d309f8ef44dfc31
jamesVectorspace <james.vectorspace@gmail.com>
2024-02-01 08:54:51 -0800

    feat: integrating backend api

---
+++ pages/api/predictions/controlnet.js
@@ -0,0 +1,47 @@
+import packageData from "../../../package.json";
+
+const REPLICATE_API_HOST = "https://api.replicate.com";
+
+export default async function handler(req, res) {
+  if (!process.env.REPLICATE_API_TOKEN) {
+    throw new Error(
+      "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+    );
+  }
+
+  const input = {
+    prompt: "A blonde girl in jeans", //req.body.prompt,
+    image: req.body.image,
+
+    // HED is slightly different
+    input_image: req.body.image,
+  };
+
+  const body = JSON.stringify({
+    input,
+    version: req.body.version,
+  });
+
+  const headers = {
+    Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+    "Content-Type": "application/json",
+    "User-Agent": `${packageData.name}/${packageData.version}`,
+  };
+
+  const response = await fetch(`${REPLICATE_API_HOST}/v1/predictions`, {
+    method: "POST",
+    headers,
+    body,
+  });
+
+  if (response.status !== 201) {
+    let error = await response.json();
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: error.detail }));
+    return;
+  }
+
+  const prediction = await response.json();
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
